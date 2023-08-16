5e86dd3763ba621c296144fd55a12b4fb8e49102
Josh Bonello <jbonello@amazon.com>
2023-08-16 14:35:17 -0500

    Have some insecure rules

--- my-tf-file.tf
+++ my-tf-file.tf
@@ -5,7 +5,7 @@ resource "aws_security_group_rule" "my-insecure-rule" {
   to_port           = "22"
   security_group_id = "sg-3bde5e7f"
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sgr
 }
 
 resource "aws_security_group_rule" "my-overriden-insecure-rule" {
